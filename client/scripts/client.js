02ccf984a7d07509c41f6e70ead807fa5d60eaf1
Anna Springfield <anna.e.springfield@gmail.com>
2017-04-07 10:02:01 -0500

    reset input fields

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -3,26 +3,12 @@ var myApp = angular.module('myApp', []);
 // controller to grab data from form
 myApp.controller('FormController', ['$scope', 'MovieService', function($scope, MovieService) {
   $scope.movies = MovieService.movies;
-  // $scope.Movie = function(title, desc, dir, time) {
-  //   this.title = title;
-  //   this.desc = desc;
-  //   this.dir = dir;
-  //   this.time = time;
-  // };
   $scope.movie = {};
-  // $scope.pushAndClear = function() {
-  //   var movie = new $scope.Movie($scope.title, $scope.desc, $scope.dir, $scope.time);
-  //   $scope.movies.pushMovie(movie);
-  //   $scope.title = '';
-  //   $scope.desc = '';
-  //   $scope.dir = '';
-  //   $scope.time = '';
-  // };
   $scope.pushAndClear = function(movie) {
     var movieCopy = angular.copy(movie);
     $scope.movies.pushMovie(movieCopy);
-    for (var key in $movie) {
-      console.log(movie.key);
+    for (var key in movie) {
+      movie[key] = '';
     }
   };
 }]);
