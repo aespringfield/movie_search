c46c08b6db410b23e346b7bf314d0e05b2dcfb6d
Anna Springfield <anna.e.springfield@gmail.com>
2017-04-09 11:33:40 -0500

    connect to API

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -2,20 +2,42 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('FormController', ['$scope', 'MovieService', function($scope, MovieService) {
   $scope.getMovieData = MovieService.getMovieData;
+  $scope.searchError = MovieService.searchError;
+  $scope.displaySearchError = MovieService.displaySearchError;
+  $scope.movie = {};
 }]);
 
 myApp.controller('DisplayController', ['$scope', 'MovieService', function($scope, MovieService) {
-  $scope.dataFromServer = MovieService.dataFromServer;
+  $scope.movieList = MovieService.movieList;
 }]);
 
 myApp.factory('MovieService', ['$http', function($http) {
-  var dataFromServer = {};
-  return {
-    dataFromServer: dataFromServer,
-    getMovieData: function() {
-      $http.get('/movieData').then(function(response) {
-        dataFromServer.response = response;
-      });
+  var movieList = [];
+  var searchError = {
+    display: false,
+    msg: "No movie found"
+  };
+  displaySearchError = function(value) {
+    if (value) {
+      searchError.display = true;
+    } else {
+      searchError.display = false;
     }
+  };
+  return {
+    movieList: movieList,
+    searchError: searchError,
+    getMovieData: function(title) {
+      displaySearchError(false);
+      $http.get('http://www.omdbapi.com/?t=' + title + '&y=&plot=full&r=json').then(function(response) {
+        console.log(response.data.Response);
+        if (response.data.Response == "True") {
+          movieList.push(response);
+        } else {
+          displaySearchError(true);
+        }
+        console.log(searchError.display);
+      });
+    },
   };
 }]);
