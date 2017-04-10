9fe0e761d3fceebf566f3202b78e14e99787d74c
Anna Springfield <anna.e.springfield@gmail.com>
2017-04-10 08:40:46 -0500

    switch back to original display

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -6,15 +6,14 @@ myApp.controller('FormController', ['$scope', 'MovieService', function($scope, M
   $scope.displaySearchError = MovieService.displaySearchError;
   $scope.addToFavorites = MovieService.addToFavorites;
   $scope.getFavorites = MovieService.getFavorites;
-  $scope.movies = MovieService.movies;
-  // $scope.currentMovie = {
-  //   display: false,
-  //   titleInput: '',
-  //   reset: function() {
-  //     this.display = false;
-  //     delete this.data;
-  //   }
-  // };
+  $scope.currentMovie = {
+    display: false,
+    titleInput: '',
+    reset: function() {
+      this.display = false;
+      delete this.data;
+    }
+  };
 }]);
 
 myApp.controller('DisplayController', ['$scope', 'MovieService', function($scope, MovieService) {
@@ -23,17 +22,22 @@ myApp.controller('DisplayController', ['$scope', 'MovieService', function($scope
   $scope.deleteFromFavorites = MovieService.deleteFromFavorites;
   $scope.getFavorites = MovieService.getFavorites;
   $scope.getFavorites();
+  $scope.printIndex = function() {
+    console.log($index);
+  };
+
 }]);
 
 myApp.factory('MovieService', ['$http', function($http) {
   var movies = {
-    resultsList = [],
     favoritesList: []
   };
+
   var searchError = {
     display: false,
     msg: "No movie found"
   };
+
   displaySearchError = function(value) {
     if (value) {
       searchError.display = true;
@@ -41,6 +45,7 @@ myApp.factory('MovieService', ['$http', function($http) {
       searchError.display = false;
     }
   };
+
   return {
     movies: movies,
     searchError: searchError,
@@ -49,29 +54,30 @@ myApp.factory('MovieService', ['$http', function($http) {
       $http.get('http://www.omdbapi.com/?t=' + currentMovie.titleInput + '&type=movie&y=&plot=short&r=json').then(function(response) {
         console.log(response.data);
         if (response.data.Response == "True") {
-          // currentMovie.titleInput = '';
-          // currentMovie.data = response.data;
-          // currentMovie.display = true;
-          movies.resultsList.push(response.data);
+          currentMovie.titleInput = '';
+          currentMovie.data = response.data;
+          currentMovie.display = true;
         } else {
-          // currentMovie.reset();
+          currentMovie.reset();
           displaySearchError(true);
         }
       });
     },
-    addToFavorites: function(index, getFavorites) {
-      var movie = movies.resultsList[index];
-      $http.post('/movie', movie).then(function(response) {
+
+    addToFavorites: function(currentMovie, getFavorites) {
+      $http.post('/movie', currentMovie.data).then(function(response) {
         console.log(response);
         getFavorites();
       });
       currentMovie.reset();
     },
+
     getFavorites: function() {
       $http.get('/movie').then(function(response){
         movies.favoritesList = response.data;
       });
     },
+
     deleteFromFavorites: function(index, getFavorites) {
       var _id = movies.favoritesList[index]._id;
       $http.delete('/movie/' + _id).then(function(response) {
